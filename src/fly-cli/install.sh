e508ee0a7fcf47d5d63cc8b96a95a9cb630a8b31
patrickcrocker <patrick.crocker@gmail.com>
2023-05-20 09:48:38 -0500

    added fly cli

---
+++ src/fly-cli/install.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+: ${URL:=https://ci.nulldriver.com}
+
+USERNAME=${USERNAME:-${_REMOTE_USER:-"automatic"}}
+
+set -e
+
+export TMPDIR=$(mktemp -d /tmp/feature.XXXXXX)
+trap "rm -rf $TMPDIR" EXIT
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+    exit 1
+fi
+
+architecture="$(dpkg --print-architecture)"
+if [ "${architecture}" != "amd64" ] && [ "${architecture}" != "arm64" ]; then
+    echo "(!) Architecture $architecture unsupported"
+    exit 1
+fi
+
+# Determine the appropriate non-root user
+if [ "${USERNAME}" = "auto" ] || [ "${USERNAME}" = "automatic" ]; then
+    USERNAME=""
+    POSSIBLE_USERS=("vscode" "node" "codespace" "$(awk -v val=1000 -F ":" '$3==val{print $1}' /etc/passwd)")
+    for CURRENT_USER in "${POSSIBLE_USERS[@]}"; do
+        if id -u "${CURRENT_USER}" >/dev/null 2>&1; then
+            USERNAME=${CURRENT_USER}
+            break
+        fi
+    done
+    if [ "${USERNAME}" = "" ]; then
+        USERNAME=root
+    fi
+elif [ "${USERNAME}" = "none" ] || ! id -u ${USERNAME} >/dev/null 2>&1; then
+    USERNAME=root
+fi
+
+apt_get_update() {
+    if [ "$(find /var/lib/apt/lists/* | wc -l)" = "0" ]; then
+        echo "Running apt-get update..."
+        apt-get update -y
+    fi
+}
+
+# Checks if packages are installed and installs them if not
+check_packages() {
+    if ! dpkg -s "$@" >/dev/null 2>&1; then
+        apt_get_update
+        apt-get -y install --no-install-recommends "$@"
+    fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+### BEGIN install
+
+# Soft version matching
+check_packages curl ca-certificates
+echo "Downloading fly..."
+curl -SL "$URL/api/v1/cli?arch=amd64&platform=linux" -o "$TMPDIR/fly"
+install "$TMPDIR/fly" /usr/local/bin/fly
+
+### END install
+
+# Clean up
+rm -rf /var/lib/apt/lists/*
+
+echo "Done!"
